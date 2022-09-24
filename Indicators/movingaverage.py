9316b856448452c7ac4e057ea4545da75ec3b63d
gorkemaltay1 <78072511+gorkemaltay1@users.noreply.github.com>
2022-09-24 22:34:29 +0300

    updating moving average

--- Indicators/movingaverage.py
+++ Indicators/movingaverage.py
@@ -1,45 +1,32 @@
 from cmath import nan
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 
-def moving_average(a,n):              # n değeri yerine girilen sayılık günlük ortalama bulduruyor
-    ret = np.cumsum(a,dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n-1:] / n
+class movingAverage:
+    def __init__(self,dataset,period=3):
+        self.dataset = dataset
+        self.period = period
 
-def plotMA(dataframe,series,df1): #İstenilen grafik tablosunu çizdiriyor
-    print(df1)
-    dataframe.plot()
-    series.plot()
-    plt.show()
-
-def nanSeries(Serie,i):
-    nanSeri=[]
-    for _ in range(i):
-        nanSeri.append(nan) #istediğimiz vakitten itibaren ortalamanın plot için gerekli boşlukları sağlayacak
-    nanSeri[(2*i+2):] = Serie
-    return nanSeri
+    def calcAverage(self):         # n değeri yerine girilen sayılık günlük ortalama bulduruyor
+        ret = np.cumsum(self.dataset,dtype=float)
+        ret[self.period:] = ret[self.period:] - ret[:-self.period]
+        return ret[self.period-1:] / self.period
 
 
+    def nanSeries(self):
+        nanSeri=[]
+        for _ in range(self.period):
+           nanSeri.append(nan) #istediğimiz vakitten itibaren ortalamanın plot için gerekli boşlukları sağlayacak
+        nanSeri[(self.period-1):] = self.calcAverage()
+        return nanSeri
+
+    def yazdir(self) -> list:
+        return self.nanSeries()
 
 
-data = pd.read_excel('movingaverage.xlsx')
-sales = data['Sales']
-dates = data['Date']
+movingAverage('dataset','period').yazdir
 
+    
+      
 
-arrMoving3=pd.Series(moving_average(np.array(sales),3))
-arrMoving5=pd.Series(moving_average(np.array(sales),5))
-arrMoving7=pd.Series(moving_average(np.array(sales),7))
-finalSeries3 = pd.Series(nanSeries(arrMoving3,3))  #3 günlük hareketli ortalama
-finalSeries5 = pd.Series(nanSeries(arrMoving5,5))  #5 günlük hareketli ortalama
-finalSeries7 = pd.Series(nanSeries(arrMoving7,7))  #7 günlük hareketli ortalama
-
-df1=pd.DataFrame(finalSeries3) 
-df1.columns = ['3-Days']
-df1['5-Day'] = finalSeries5
-df1['7-Day'] = finalSeries7
-plotMA(sales,finalSeries5,df1)
 
