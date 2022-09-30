3b5769b7716755acd01bdaff3b4a09544c1ded37
Anıl Mehmet Uyar <109759873+ANL3@users.noreply.github.com>
2022-09-30 16:03:10 +0300

    Update macd.py
    All code translated to English. Unnecessary lines deleted.

--- Indicators/macd.py
+++ Indicators/macd.py
@@ -1,50 +1,48 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 import pandas as pd
 
 class MACD:
    
     def __init__(self, data_close):
         self.data_close = data_close
+        #we only need to the closure data
 
     def MovingAverageConvergenceDivergence(self,data_close):
    
-        #Ema_12 için ilk değer ilk 12 günün ortalaması olacak.
-        #Diğer değerler aşağıdaki fonksiyona göre hesaplanmaktadır.
-        ortalama_12=sum(self.data_close[0:12])/12
-        Ema_12=[ortalama_12]            
+        #Ema_12 is essential for the MACD, this "12" can be changable, but sorurces take "12".
+        #For Ema_12 first we need to mean of first 12 days.
+        #Other values of Ema_12 comes from the below calculation. Ema_12 values listed in "for" loop.
+        mean_12=sum(self.data_close[0:12])/12
+        Ema_12=[mean_12]            
         for i in range(54):
             Ema_12.append(self.data_close[i+12]*(0.15384615384615385)+(Ema_12[i]*(0.8461538461538461)))
 
-        #Ema_26 için ilk değer ilk 26 günün ortalaması olacak.
-        #Diğer değerler aşağıdaki fonksiyona göre hesaplanmaktadır.
-        ortalama_26=sum(data_close[0:26])/26
-        Ema_26=[ortalama_26]
+        #Ema_26 is essential for the MACD, this "26" can be changable, but sources take "26". 
+        #For Ema_26 first we need to mean of first 26 days.
+        #Other values of Ema_26 comes from the below calculation. Ema_26 values listed in "for" loop.
+        mean_26=sum(data_close[0:26])/26
+        Ema_26=[mean_26]
         for i in range(40):
             Ema_26.append(self.data_close[i+26]*(0.07407407407407407)+(Ema_26[i]*(0.9259259259259259)))
    
    
-        #MACD için değerler aynı güne ait Ema_12 ve Ema_26 değerlerinin farkı alınarak hesaplanmaktadır.
+        #MACD values calculated with (Ema_12 - Ema_26). Datas must be taken from the same day. Because of that i add "14" for Ema_12 values.
         calc_MACD=[]
         for i in range(41):
             calc_MACD.append(Ema_12[14+i]-Ema_26[i])
    
-        #Signal için ilk değer ilk 9 günün MACD verilerinin ortalamsıdır.
-        #Diğer değerler aşağıdaki fonksiyona göre hesaplanmaktadır.
-        signal_ort=sum(calc_MACD[0:9])/9
-        Signal=[signal_ort]
+        #For Signal values, first value is the mean of first 9 MACD values.
+        #Other values of Signal comes from the below calculation.
+        signal_mean=sum(calc_MACD[0:9])/9
+        Signal=[signal_mean]
         for i in range(32):
             Signal.append(calc_MACD[i+9]*(0.2)+(Signal[i]*(0.8)))
    
-        #Histogram için aynı güne ait MACD ve Signal değerlerinin farkı alınarak hesaplama yapılmaktdır.
+        #Same day of MACD(starting from 8) and Signal values we calculate Histogram values with (MACD - Signal).
         Histogram=[]
         for i in range(33):
             Histogram.append(calc_MACD[8+i]-Signal[i])
    
+        #If histogram value turns negative to positive that mean we can buy the stock, else we can sell the stock. In this code these are printed.
         for i in range(32):
             if Histogram[i+1]>0 and Histogram[i]<0:
                 print("Date of "+f"{i+1}","Buy")
@@ -63,17 +61,6 @@ class MACD:
        
         return df
 
-
-
-path=r"/Users/anilmehmetuyar/Downloads/MACD-Data.xlsx"
-data=pd.read_excel(path)
-data_close = data['Close'].to_list()
+#data_close is the closure data. Then we put this data(list type) and print the result. This result is the matrix of the all calculations. 
 Result=MACD(data_close)
 print(Result.MovingAverageConvergenceDivergence(data_close))
-
-
-# In[ ]:
-
-
-
-
