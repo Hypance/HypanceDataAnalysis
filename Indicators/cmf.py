dc7ea5816d4d3129b3da9199110504589a1d20b9
Bahar <bhrakay235@gmail.com>
2022-09-30 13:42:37 +0300

    cmf is updated

--- Indicators/cmf.py
+++ Indicators/cmf.py
@@ -1,27 +1,48 @@
 import pandas as pd
 
 class CMF:
-    def __init__(self,close,low,high,volume,data):
+
+    """
+    This class calculate the cmf for 20 period.
+    ...
+    Attributes
+    ----------
+    close : list
+    low : list
+    high : list
+    volume: list
+
+    Methods
+    -------
+    money_flow_multiplier(close,low,high):
+        Returns the mfm.
+
+    money_flow_volume(mfm, volume):
+        Returns the mfv.
+
+    cmf(money_flow_multiplier,money_flow_volume,data):
+        Returns the cmf. 
+
+    """
+    
+    def __init__(self,close:list,low:list,high:list,volume:list,data):
         self.close = close
         self.low = low
         self.high = high
         self.volume = volume
         self.data = data
         
-    #1. Money Flow Multiplier = [(Close  -  Low) - (High - Close)] /(High - Low) 
     def money_flow_multiplier(close,low,high):
         mfm = ((close - low) - (high - close)) / (high - low)
         return mfm
 
-    # 2. Money Flow Volume = Money Flow Multiplier x Volume for the Period
     def money_flow_volume(mfm, volume):
         mfv = mfm * volume
         return mfv
     
-    # 3. 20-period CMF = 20-period Sum of Money Flow Volume / 20 period Sum of Volume 
     def cmf(money_flow_multiplier,money_flow_volume,data):
-        result1 = money_flow_multiplier(data.loc[:,"Close"],data.loc[:,"Low"],data.loc[:,"High"])
-        result2 = money_flow_volume(result1, data.loc[:, "Volume"])
-        cmf = result2.rolling(window=20).sum() / data["Volume"].rolling(window=20).sum()
+        mfm = money_flow_multiplier(data.loc[:,"Close"],data.loc[:,"Low"],data.loc[:,"High"])
+        mfv = money_flow_volume(mfm, data.loc[:, "Volume"])
+        cmf = mfv.rolling(window=20).sum() / data["Volume"].rolling(window=20).sum()
         return cmf
 
