19ea169646d88502720034c64f1a0cb51fb647bf
Anıl Mehmet Uyar <109759873+ANL3@users.noreply.github.com>
2022-10-01 19:18:07 +0300

    Update atr.py
    Lines cleared.

--- Indicators/atr.py
+++ Indicators/atr.py
@@ -7,15 +7,16 @@
 #         self.data_all = data_all
 #       
 # 
-#     def AverageTrueRange(self,data_all):
+#     def AverageTrueRange(self):
 #    
-#         #True Range için yapılacak hesaplamaları TR listesine atalım.
+#         #First calculating true ranges. Substarcting high,low and close values.
 #         TR=[abs(data_all[1][0]-data_all[2][0])]            
 #         for i in range(1,30):
 #             TR.append(max(abs(data_all[0][i-1]-data_all[1][i]),
 #                          abs(data_all[0][i-1]-data_all[2][i]),
 #                          abs(data_all[1][i]-data_all[2][i])))
-#             
+#         
+#         At the end  we calculating avarage ture ranges.
 #         ATR=[]
 #         ATR.append(np.mean(TR[0:14]))
 #         for i in range(16):
@@ -33,5 +34,4 @@
 #         return df[13:].reset_index()
 #
 # data_all=np.array([data_close,data_high,data_low])
-# Result=ATR(data_all)
-# print(Result.AverageTrueRange(data_all))
+# print(ATR(data_all).AverageTrueRange())
