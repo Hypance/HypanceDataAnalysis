af5357ae77710a9faf8503ab6c48104ed13b939e
Toygar Aksoy <toygar.aksoy@gmail.com>
2022-10-05 00:03:03 +0300

    setup.py is fixed

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='HypanceDataAnalysis',
+    version='0.0.2',
+    author='Toygar Aksoy',
+    author_email='toygar.aksoy@gmail.com',
+    description='Indicators for Hypance Project',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Hypance/HypanceDataAnalysis',
+    # project_urls = {
+    #     "Bug Tracker": "https://github.com/Hypance/HypanceDataAnalysis/issues"
+    # },
+    license='MIT',
+    packages=['hyta'],
+    install_requires=['pandas', 'numpy'],
+)
