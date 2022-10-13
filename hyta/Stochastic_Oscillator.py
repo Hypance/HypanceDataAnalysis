163d7cc8bcc7e1987498a70051847a6740d8105e
Esra Ovalı <esra.ovali@gmail.com>
2022-10-13 23:08:40 +0300

    StochasticOscillator is updated

--- hyta/Stochastic_Oscillator.py
+++ hyta/Stochastic_Oscillator.py
@@ -7,22 +7,28 @@ class StochasticOscillator:
         self.list=list
         self.periods=periods
 
-    def add_stochastic_oscillator(self):
+    def High_Low_Stoch(self):
         df = pd.DataFrame([self.list])
 
         high_roll = df["High"].rolling(self.periods).max()
         low_roll = df["Low"].rolling(self.periods).min()
+        return df
 
     # Fast stochastic indicator
+    
+    def Fast_Stochastic():
         num = df["Close"] - low_roll
         denom = high_roll - low_roll
         df["%K"] = (num / denom) * 100
+        return  df["%K"]
 
     # Slow stochastic indicator
+    
+    def Slow_Stochastic():
         df["%D"]= df["%K"].rolling(3).mean()
         return df["%D"]
     
-    def print(self):
-        return self.add_stochastic_oscillator()
+    def StochasticOscillator(self):
+        return self.High_Low_Stoch()
 
 
