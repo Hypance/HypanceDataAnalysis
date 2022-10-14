63de39846b233d2431691b7d7be8d71ae49d1420
Bahar <57152233+Bahar-K@users.noreply.github.com>
2022-10-14 22:31:21 +0300

    Add files via upload

--- Tests/test_cmf.py
+++ Tests/test_cmf.py
@@ -8,20 +8,21 @@ class TestCmf(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data = pd.read_excel('cs-cmf.xls')
-    
+
     @classmethod
     def tearDownClass(cls):
         del cls.data
-    
+
     def setUp(self) -> None:
         self.close = self.data["Close"]
         self.low = self.data["Low"]
         self.high = self.data["High"]
         self.volume = self.data["Volume"]
 
-        b = CMF(self.close,self.low,self.high,self.volume)
-        self.mfm_result = b.money_flow_multiplier(self.close,self.low,self.high)
-        self.mfv_result = b.money_flow_volume(self.mfm_result,self.volume)
+        cmf_data = CMF(self.close,self.low,self.high,self.volume)
+        self.mfm_result = cmf_data.money_flow_multiplier()
+        self.mfv_result = cmf_data.money_flow_volume()
+        self.cmf_result = cmf_data.cmf()
 
     def test_mfm(self):
         self.assertAlmostEqual(self.mfm_result[1],self.data["MF Multiplier"][1])
@@ -34,13 +35,11 @@ class TestCmf(unittest.TestCase):
         self.assertIsNotNone(self.mfv_result[1])
 
     def test_cmf(self):
-        a = CMF(self.close,self.low,self.high,self.volume)
-        cmf_result = a.cmf()
-        #self.assertEqual(type(cmf_result[1]),np.nan)
-        self.assertIsInstance(cmf_result,pd.Series)
-        np.testing.assert_equal(cmf_result[1],np.nan)
-        self.assertGreater(cmf_result[23],-1)
-        self.assertLess(cmf_result[23],1)
+        self.assertIsInstance(self.cmf_result,pd.Series)
+        np.testing.assert_equal(self.cmf_result[1],np.nan)
+        self.assertGreater(np.nanmin(self.cmf_result),-1)
+        self.assertLess(np.nanmax(self.cmf_result),1)
+
 
 if __name__ == "__main__":
     unittest.main()
