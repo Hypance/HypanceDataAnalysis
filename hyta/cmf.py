cd3442f288fe2dff186a34f387d30886528b9ae1
Bahar <57152233+Bahar-K@users.noreply.github.com>
2022-10-14 22:28:07 +0300

    cmf.py is updated

--- hyta/cmf.py
+++ hyta/cmf.py
@@ -3,7 +3,7 @@ import pandas as pd
 class CMF:
 
     """
-    This class calculate the cmf for 20 period.
+    This class calculate the cmf for default 20-days period.
     ...
     Attributes
     ----------
@@ -25,34 +25,30 @@ class CMF:
 
     """
     
-    def __init__(self,close:pd.Series,low:pd.Series,high:pd.Series,volume:pd.Series,period=20):
+    def __init__(self,close:pd.Series,low:pd.Series,high:pd.Series,volume:pd.Series,period:int=20):
         self.close = close
         self.low = low
         self.high = high
         self.volume = volume
         self.period = period
         
-    def money_flow_multiplier(self):
+    def money_flow_multiplier(self) -> pd.Series:
         mfm = ((self.close - self.low) - (self.high - self.close)) / (self.high - self.low)
         try:
             self.low/self.high
         except ZeroDivisionError as e:
             return "There is a data problem."
-            
+        
         return mfm
 
-    def money_flow_volume(self):
-        mfv = self.mfm * self.volume
+    def money_flow_volume(self) -> pd.Series:
+        mfv = self.money_flow_multiplier() * self.volume
         return mfv
     
-    def cmf(self):
-        close = self.close
-        low = self.low
-        high = self.high
+    def cmf(self) -> pd.Series:
         volume = self.volume
         period = self.period
 
-        mfm = self.money_flow_multiplier(close,low,high)
-        mfv = self.money_flow_volume(mfm,volume)
+        mfv = self.money_flow_volume()
         cmf = mfv.rolling(window=period).sum() / volume.rolling(window=period).sum()
         return cmf
