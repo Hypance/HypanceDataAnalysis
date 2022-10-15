b3cbe5fadf0509928779c0922b08858454075665
Anil Mehmet Uyar <anilmehmet.uyar@gmail.com>
2022-10-15 20:38:41 +0300

    set up function deleted and  names of results changed

--- Tests/test_atr.py
+++ Tests/test_atr.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import pandas as pd
-from Test.ATR import ATR
+from hyta.atr import ATR
 
 class TestAverageTrueRange(unittest.TestCase):
     
@@ -17,53 +17,51 @@ class TestAverageTrueRange(unittest.TestCase):
         
     @classmethod
     def tearDownClass(cls):
-        del cls.data   
+        del cls.data  
+        del cls.data_close
+        del cls.data_high
+        del cls.data_low
+        del cls.data_all   
         
-    def setUp(self):
-        self.true_value1=14
-        self.true_value2=20
-        self.result00 = ATR(self.data_all)
-        self.result01 = ATR(self.data_all,20)
+    def test_tr(self):
+        self.result = ATR(self.data_all)
+        self.result_tr=ATR(self.data_all).true_range()
         
-        self.result1=ATR(self.data_all).TrueRange()
-        self.result2=ATR(self.data_all).AverageTrueRange()
-                
-        self.result3=ATR(self.data_all,20).TrueRange()
-        self.result4=ATR(self.data_all,20).AverageTrueRange()
-        
-        self.result5=ATR(self.data_all,0).AverageTrueRange()   
-                
-    def test_data(self):
-        for i in range(len(self.data_all[2])):
-            self.assertLess(abs(self.data_all[1][i]),abs(self.data_all[0][i]))
-            self.assertLessEqual(abs(self.data_all[2][i]),abs(self.data_all[0][i]))
-        
-    def test_tr(self):        
-        self.assertEqual(self.result1[0], self.result00.TrueRange()[0])
-        self.assertEqual(self.result1[2], self.result00.TrueRange()[2])    
-        self.assertEqual(self.result1[-1], self.result00.TrueRange()[-1])
-        
-        self.assertEqual(len(self.result1), len(self.result00.TrueRange()))
+        self.assertEqual(self.result_tr[0], self.result.true_range()[0])
+        self.assertEqual(self.result_tr[2], self.result.true_range()[2])    
+        self.assertEqual(self.result_tr[-1], self.result.true_range()[-1])
+       
+        self.assertEqual(len(self.result_tr), len(self.result.true_range()))
         
               
-    def test_atr(self):        
-        self.assertEqual(self.result2[0], self.result00.AverageTrueRange()[0])
-        self.assertEqual(self.result2[2], self.result00.AverageTrueRange()[2])
-        self.assertEqual(self.result2[-1], self.result00.AverageTrueRange()[-1])
+    def test_atr(self):  
+        self.result = ATR(self.data_all)
+        self.result_atr=ATR(self.data_all).average_true_range()
+
+        self.assertEqual(self.result_atr[0], self.result.average_true_range()[0])
+        self.assertEqual(self.result_atr[2], self.result.average_true_range()[2])
+        self.assertEqual(self.result_atr[-1], self.result.average_true_range()[-1])
    
-        self.assertEqual(len(self.result2), len(self.result00.AverageTrueRange())) 
-         
+        self.assertEqual(len(self.result_atr), len(self.result.average_true_range()))        
     
     def test_tr_atr(self):
-        self.assertGreater(len(self.result00.TrueRange()), len(self.result00.AverageTrueRange())) 
-        self.assertEqual(len(self.result00.TrueRange())-len(self.result00.AverageTrueRange()),self.true_value1-1)  
+        self.result = ATR(self.data_all)
 
+        self.true_value1=14
+        self.assertGreater(len(self.result.true_range()), len(self.result.average_true_range()))
+        self.assertEqual(len(self.result.true_range())-len(self.result.average_true_range()),self.true_value1-1)  
         
     def test_tr_atr_true_value_20(self):
-        self.assertEqual(len(self.result3)-len(self.result4),self.true_value2-1) 
+        self.result_tr_20=ATR(self.data_all,20).true_range()
+        self.result_atr_20=ATR(self.data_all,20).average_true_range()
+        self.true_value2=20
+
+        self.assertEqual(len(self.result_tr_20)-len(self.result_atr_20),self.true_value2-1) 
         
     def test_atr_none(self):
-        np.testing.assert_equal(self.result5[-1],np.nan)
+        self.result_atr_none=ATR(self.data_all,0).average_true_range()  
+
+        np.testing.assert_equal(self.result_atr_none[-1],np.nan)
         
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
