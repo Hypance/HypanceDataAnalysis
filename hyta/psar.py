a33b242c1533edc98bac9a32cad16c0a9e2cdc92
Anil Mehmet Uyar <anilmehmet.uyar@gmail.com>
2022-10-17 16:15:11 +0300

    format is arranged

--- hyta/psar.py
+++ hyta/psar.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
 import numpy as np
 
@@ -16,66 +10,72 @@ class ParabolicSAR:
     
     Attributes
     --------------- 
-    data_high : list
-    data_low : list
-    data_close : list
+    high : list
+    low : list
+    close : list
     '''
    
-    def __init__(self,data_high:list,data_low:list,data_close:list):
-        self.data_high = data_high
-        self.data_low = data_low
-        self.data_close = data_close
+    def __init__(self,high:list,low:list,close:list):
+        self.high = high
+        self.low = low
+        self.close = close
       
     def parabolic_sar(self) -> list:
         
         #first, setting up initial values 
+
         #ep is a list of extremum points
-        ep=[data_low[0]]  
+        ep=[low[0]]  
+
         #psar is a list of parabolic stop and reverse values
-        psar=[data_high[0]] 
-        #acc point is a accumulation point list
+        psar=[high[0]] 
+
+        #acc point is a accumulation point list(maximum accumulation point is 0.2)
         acc_point=[.02]
+
         #trend is a list of trend of graphic(falling and rising)
         trend=["Falling"]
+
         #calc_psar is a list of calculation of below function
         calc_psar=[(psar[0]-ep[0])*acc_point[0]]
+
         #initial_psar is a list of values determines actual psar
         initial_psar=[]
         
-        for i in range(len(data_high)-1):
+        for i in range(len(high)-1):
             #setting initial psar values according to trend
             if trend[i]=="Falling":
                 try:
-                    initial_psar.append(max((psar[i]-calc_psar[i]),np.array(data_high[i-1:i+1]).max()))
+                    initial_psar.append(max((psar[i]-calc_psar[i]),np.array(high[i-1:i+1]).max()))
                 except ValueError:
-                    initial_psar.append(max((psar[i]-calc_psar[i]),np.array(data_high[0:1]).max()))
+                    initial_psar.append(max((psar[i]-calc_psar[i]),np.array(high[0:1]).max()))
             elif trend[i]=="Rising":
                 try:
-                    initial_psar.append(min((psar[i]-calc_psar[i]),np.array(data_low[i-1:i+1]).min()))
+                    initial_psar.append(min((psar[i]-calc_psar[i]),np.array(low[i-1:i+1]).min()))
                 except ValueError:
-                    initial_psar.append(max((psar[i]-calc_psar[i]),np.array(data_low[0:1]).min()))
+                    initial_psar.append(max((psar[i]-calc_psar[i]),np.array(low[0:1]).min()))
                     
-            #setting psar values according to trend,initial_psar and data 
-            if trend[i]=="Falling" and data_high[i+1]<initial_psar[i]:
+            #setting psar values according to trend,initial_psar and given data 
+            if trend[i]=="Falling" and high[i+1]<initial_psar[i]:
                 psar.append(initial_psar[i])
-            elif trend[i]=="Falling" and data_high[i+1]>=initial_psar[i]:
+            elif trend[i]=="Falling" and high[i+1]>=initial_psar[i]:
                 psar.append(ep[i])
-            elif trend[i]=="Rising" and data_low[i+1]>initial_psar[i]:
+            elif trend[i]=="Rising" and low[i+1]>initial_psar[i]:
                 psar.append(initial_psar[i])
-            elif trend[i]=="Rising" and data_low[i+1]<=initial_psar[i]:
+            elif trend[i]=="Rising" and low[i+1]<=initial_psar[i]:
                 psar.append(ep[i])
                 
             #decide on the trend according to psar and close data
-            if psar[i+1]>data_close[i+1]:
+            if psar[i+1]>close[i+1]:
                 trend.append("Falling")
             else:
                 trend.append("Rising")
                 
             #setting extremum point    
             if trend[i+1]=="Falling":
-                ep.append(min(ep[i],data_low[i+1]))
+                ep.append(min(ep[i],low[i+1]))
             else:
-                ep.append(max(ep[i],data_high[i+1]))
+                ep.append(max(ep[i],high[i+1]))
                 
             #decide on the accumulation point    
             if trend[i]==trend[i+1] and ep[i]!=ep[i+1] and acc_point[i]<0.2:
@@ -92,19 +92,3 @@ class ParabolicSAR:
 
                 
         return psar
-    
-
-
-path=r"/Users/anilmehmetuyar/Downloads/PSAR.xlsx"
-data=pd.read_excel(path)
-data_close = data['Close'].to_list()
-data_high = data['High'].to_list()
-data_low = data['Low'].to_list()
-print(ParabolicSAR(data_high,data_low,data_close).parabolic_sar())
-
-
-# In[ ]:
-
-
-
-
