7a695ab11176866bf23cd03db9e7dd919abe5bcc
Anil Mehmet Uyar <anilmehmet.uyar@gmail.com>
2022-10-17 15:11:04 +0300

    test changes done and excel file added

--- Tests/test_atr.py
+++ Tests/test_atr.py
@@ -5,52 +5,58 @@ from hyta.atr import ATR
 
 class TestAverageTrueRange(unittest.TestCase):
     
-    path=r"/Users/anilmehmetuyar/Downloads/ATR.xlsx"
+    path=r"/Users/anilmehmetuyar/Downloads/ATR.xls"
 
     @classmethod
     def setUpClass(cls):
         cls.data=pd.read_excel(cls.path)
-        cls.data_close = cls.data['Close'].to_list()
         cls.data_high = cls.data['High'].to_list()
-        cls.data_low = cls.data['Low'].to_list()
+        cls.data_low = cls.data['Low'].to_list()        
+        cls.data_close = cls.data['Close'].to_list()
+        cls.result_tr = cls.data['TR'].to_list()
+        cls.result_atr = cls.data['ATR'].to_list()
         cls.data_all=np.array([cls.data_high,cls.data_low,cls.data_close])    
         
     @classmethod
     def tearDownClass(cls):
-        del cls.data  
-        del cls.data_close
+        del cls.data
         del cls.data_high
-        del cls.data_low
+        del cls.data_low  
+        del cls.data_close
+        del cls.result_tr
+        del cls.result_atr
         del cls.data_all   
-        
+    
+    #below function is testing true range results with respect to excel spreadsheet    
     def test_tr(self):
         self.result = ATR(self.data_all)
-        self.result_tr=ATR(self.data_all).true_range()
         
         self.assertEqual(self.result_tr[0], self.result.true_range()[0])
         self.assertEqual(self.result_tr[2], self.result.true_range()[2])    
-        self.assertEqual(self.result_tr[-1], self.result.true_range()[-1])
+        self.assertEqual(round(self.result_tr[-1],2), round(self.result.true_range()[-1],2))
        
         self.assertEqual(len(self.result_tr), len(self.result.true_range()))
         
-              
+    #below function is testing average true range results with respect to excel spreadsheet           
     def test_atr(self):  
         self.result = ATR(self.data_all)
-        self.result_atr=ATR(self.data_all).average_true_range()
+	self.true_value1=14
 
-        self.assertEqual(self.result_atr[0], self.result.average_true_range()[0])
-        self.assertEqual(self.result_atr[2], self.result.average_true_range()[2])
+        self.assertEqual(round(self.result_atr[self.true_value1+1],2), round(self.result.average_true_range()[2],2))
+        self.assertEqual(round(self.result_atr[2],2), round(self.result.average_true_range()[2],2))
         self.assertEqual(self.result_atr[-1], self.result.average_true_range()[-1])
    
-        self.assertEqual(len(self.result_atr), len(self.result.average_true_range()))        
+        self.assertEqual(len(self.result_atr)-self.true_value1+1, len(self.result.average_true_range()))        
     
+    #below function is testing corresponding tr and atr results
     def test_tr_atr(self):
         self.result = ATR(self.data_all)
-
         self.true_value1=14
+
         self.assertGreater(len(self.result.true_range()), len(self.result.average_true_range()))
         self.assertEqual(len(self.result.true_range())-len(self.result.average_true_range()),self.true_value1-1)  
-        
+    
+    #below functions is testing for getting different true_value 
     def test_tr_atr_true_value_20(self):
         self.result_tr_20=ATR(self.data_all,20).true_range()
         self.result_atr_20=ATR(self.data_all,20).average_true_range()
