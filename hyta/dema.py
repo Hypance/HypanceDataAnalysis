cadc33941f9b400d9da46d39ae970e790facbf10
esramelikecakir <esramelike10@gmail.com>
2022-10-25 23:24:05 +0300

    Create dema.py

---
+++ hyta/dema.py
@@ -0,0 +1,23 @@
+from hyta.ema import EMA
+import numpy as np
+import pandas as pd
+import openpyxl
+import xlrd
+class DEMA:
+
+    def __init__(self, period=9):
+        self.period = period - 1
+        self.close = close
+
+    """
+    Calculating first EMA for DEMA
+    """
+    def ema(self) -> np.array:
+        return EMA(self.close ,self.period).calc_ema()
+
+    """
+    Calculating dema by using function --> 2*Ema - Ema of Ema 
+    """
+
+    def dema(self) -> np.array:
+        return (2 * EMA(self.close,self.period).calc_ema() ) - (EMA(EMA(self.close,self.period).calc_ema(),self.period).calc_ema())
