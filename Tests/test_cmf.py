f4afbbf7398dbaec5e4c0ea10b727d7a82a18239
Bahar <bhrakay235@gmail.com>
2022-10-27 23:10:27 +0300

    test.cmf is upgraded

--- Tests/test_cmf.py
+++ Tests/test_cmf.py
@@ -1,5 +1,5 @@
 import unittest
-from cmf import CMF 
+from hyta.cmf import CMF 
 import pandas as pd
 import numpy as np
 
@@ -7,7 +7,7 @@ class TestCmf(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.data = pd.read_excel('files/cs-cmf.xls')
+        cls.data = pd.read_excel('Files/cs-cmf.xls')
 
     @classmethod
     def tearDownClass(cls):
@@ -18,28 +18,29 @@ class TestCmf(unittest.TestCase):
         self.low = self.data["Low"]
         self.high = self.data["High"]
         self.volume = self.data["Volume"]
-
-        cmf_data = CMF(self.close,self.low,self.high,self.volume)
-        self.mfm_result = cmf_data.money_flow_multiplier()
-        self.mfv_result = cmf_data.money_flow_volume()
-        self.cmf_result = cmf_data.cmf()
+        self.cmf_data = CMF(self.close,self.low,self.high,self.volume)
 
     def test_mfm(self):
-        self.assertAlmostEqual(self.mfm_result[1],self.data["MF Multiplier"][1])
-        self.assertNotEqual(self.mfm_result[1],0.82323)
+        self.mfm_result = self.cmf_data.money_flow_multiplier()
         self.assertIsNotNone(self.mfm_result[1])
+        self.assertAlmostEqual(self.mfm_result[1],self.data["MF Multiplier"][1])
+        self.assertAlmostEqual(self.mfm_result[3],self.data["MF Multiplier"][3])
+        self.assertNotEqual(self.mfm_result[1],0.82323)
 
     def test_mfv(self):
-        self.assertAlmostEqual(self.mfv_result[1],self.data["MF Volume"][1])
-        self.assertNotEqual(self.mfv_result[1],0.23000)
+        self.mfv_result = self.cmf_data.money_flow_volume()
         self.assertIsNotNone(self.mfv_result[1])
+        self.assertAlmostEqual(self.mfv_result[1],self.data["MF Volume"][1])
+        self.assertAlmostEqual(self.mfv_result[5],self.data["MF Volume"][5])
+        self.assertNotEqual(self.mfv_result[1],0.23000)
 
     def test_cmf(self):
+        self.cmf_result = self.cmf_data.cmf()
         self.assertIsInstance(self.cmf_result,pd.Series)
         np.testing.assert_equal(self.cmf_result[1],np.nan)
         self.assertGreater(np.nanmin(self.cmf_result),-1)
         self.assertLess(np.nanmax(self.cmf_result),1)
 
-
 if __name__ == "__main__":
     unittest.main()
+
