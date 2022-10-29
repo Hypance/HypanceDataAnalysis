3c3e16056e173bf611e4ef0c85a5af613cbfc157
Bahar <57152233+Bahar-K@users.noreply.github.com>
2022-10-29 20:30:00 +0300

    Cmf.py (#42)
    * cmf is added
    
    * Update cmf.py
    
    * cmf.py is updated
    
    * Update cmf.py
    
    * cmf is updated
    
    * list type is changed to pd.Series
    
    * updated
    
    * test_cmf.py is added
    
    * cmf is upgraded
    
    * cmf is updated
    
    * cmf.py is updated
    
    * test_cmf is deleted.
    
    This branch is cmf.py so test_cmf file is deleted.
    
    * cmf
    
    * cmf.py is changed
    
    cmf.py is changed
    
    Co-authored-by: Bahar <57152233+Bahra23@users.noreply.github.com>

--- hyta/cmf.py
+++ hyta/cmf.py
@@ -3,14 +3,15 @@ import pandas as pd
 class CMF:
 
     """
-    This class calculate the cmf for 20 period.
+    This class calculate the cmf for default 20-days period.
     ...
     Attributes
     ----------
-    close : list
-    low : list
-    high : list
-    volume: list
+    close : pandas.Series
+    low   : pandas.Series
+    high  : pandas.Series
+    volume: pandas.Series
+    period: pandas.Series
 
     Methods
     -------
@@ -24,25 +25,30 @@ class CMF:
         Returns the cmf. 
 
     """
-    
-    def __init__(self,close:list,low:list,high:list,volume:list,data):
+    def __init__(self,close:pd.Series,low:pd.Series,high:pd.Series,volume:pd.Series,period:int=20):
         self.close = close
         self.low = low
         self.high = high
         self.volume = volume
-        self.data = data
+        self.period = period
         
-    def money_flow_multiplier(close,low,high):
-        mfm = ((close - low) - (high - close)) / (high - low)
+    def money_flow_multiplier(self) -> pd.Series:
+        mfm = ((self.close - self.low) - (self.high - self.close)) / (self.high - self.low)
+        try:
+            self.low/self.high
+        except ZeroDivisionError as e:
+            print("Value of high is zero:", e)
+        
         return mfm
 
-    def money_flow_volume(mfm, volume):
-        mfv = mfm * volume
+    def money_flow_volume(self) -> pd.Series:
+        mfv = self.money_flow_multiplier() * self.volume
         return mfv
     
-    def cmf(money_flow_multiplier,money_flow_volume,data):
-        mfm = money_flow_multiplier(data.loc[:,"Close"],data.loc[:,"Low"],data.loc[:,"High"])
-        mfv = money_flow_volume(mfm, data.loc[:, "Volume"])
-        cmf = mfv.rolling(window=20).sum() / data["Volume"].rolling(window=20).sum()
-        return cmf
+    def cmf(self) -> pd.Series:
+        volume = self.volume
+        period = self.period
 
+        mfv = self.money_flow_volume()
+        cmf = mfv.rolling(window=period).sum() / volume.rolling(window=period).sum()
+        return cmf
