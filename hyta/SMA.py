76dd142e03136bd5948254d59556762ddeb3ced9
Esra Ovalı <esra.ovali@gmail.com>
2022-10-29 14:03:09 +0300

    SMA is updated

--- hyta/SMA.py
+++ hyta/SMA.py
@@ -1,28 +1,19 @@
 import os
-from binance.client import Client
 import pprint
 import pandas as pd     # needs pip install if not installed
-import numpy as np
-import matplotlib.pyplot as plt   # needs pip install if not installed
  
-
 class Sma:
 
-    def __init__(self,data,smaPeriod):
-        self.data=data
-        self.smaPeriod=smaPeriod
+    def __init__(self,close):
+        self.df=pd.DataFrame(data={"close":close})
 
-    def __get_hourly_dataframe():
-        pass
-
-    
     def main():
         Sma()
 
-    def Sma():
-        symbol_df = __get_hourly_dataframe()
+    def Sma(self):
+        df = pd.DataFrame([self.df])
+        # calculate 3 moving average using Pandas
+        df['3sma'] = df['close'].rolling(3).mean()
         # calculate 5 moving average using Pandas
-        symbol_df['5sma'] = symbol_df['close'].rolling(5).mean()
-        # calculate 15 moving average using Pandas
-        symbol_df['15sma'] = symbol_df['close'].rolling(15).mean()
-
+        df['5sma'] = df['close'].rolling(5).mean()
+        return df
