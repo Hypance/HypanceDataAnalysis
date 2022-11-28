b0fc2098ce995b1415f12de5f84eacd1d738a0c6
Ugur Aydin <uguraydin7541@gmail.com>
2022-11-28 22:59:34 +0300

    the result test was diveded

--- Tests/test_ichimoku_cloud.py
+++ Tests/test_ichimoku_cloud.py
@@ -20,7 +20,7 @@ class TestIchimokuCloud(unittest.TestCase):
         cls.spb = cls.data['senkau span b'].to_list()
         cls.cs = cls.data['cikou span'].to_list()
         cls.all = [pd.Series([cls.ts[e],cls.ks[e],cls.spa[e],cls.spb[e],cls.cs[e]],["Tenkan Sen","Kijun Sen","Senkou Span A","Senkou Span B","Cikou Span"]) for e in range(len(cls.close))]
-
+        cls.ic = IchimokuCloud(cls.close,cls.low,cls.high)
     @classmethod
     def tearDownClass(cls):
         del cls.data
@@ -33,38 +33,34 @@ class TestIchimokuCloud(unittest.TestCase):
         del cls.spb
         del cls.cs
         del cls.all
-        
+        del cls.ic       
+  
+
+    def test_tenka_sen(self):        
+        self.assertAlmostEqual(self.ic.ichimoku_cloud()[0],self.all[-1][0])
+        self.assertIsInstance(self.ic.ichimoku_cloud()[0],float)
+        self.assertGreater(self.ic.ichimoku_cloud()[0],0)
+    def test_kijun_sen(self):        
+        self.assertAlmostEqual(self.ic.ichimoku_cloud()[1],self.all[-1][1])
+        self.assertIsInstance(self.ic.ichimoku_cloud()[1],float)
+        self.assertGreater(self.ic.ichimoku_cloud()[1],0)
+    def test_sekou_span_a(self):        
+        self.assertAlmostEqual(self.ic.ichimoku_cloud()[2],self.all[-1][2])
+        self.assertIsInstance(self.ic.ichimoku_cloud()[2],float)
+        self.assertGreater(self.ic.ichimoku_cloud()[2],0)
+    def test_senkou_span_b(self):        
+        self.assertAlmostEqual(self.ic.ichimoku_cloud()[3],self.all[-1][3])
+        self.assertIsInstance(self.ic.ichimoku_cloud()[3],float)
+        self.assertGreater(self.ic.ichimoku_cloud()[3],0)
+    def test_cikou_span_a(self):       
+        self.assertAlmostEqual(self.ic.ichimoku_cloud()[4],self.all[-1][4])
+        self.assertIsInstance(self.ic.ichimoku_cloud()[4],float)
+        self.assertGreater(self.ic.ichimoku_cloud()[4],0)
+
+    def test_ichimoku_cloud(self):
+        self.assertIsInstance(self.ic.ichimoku_cloud(), pd.Series)
+        self.assertEqual(len(self.ic.ichimoku_cloud()),len(self.all[-1]))
     
-    def test_ichimoku_cloud_result(self):
-        
-
-        ic = IchimokuCloud(self.close,self.low,self.high)
-
-        #test_tenka_sen(self):        
-        self.assertAlmostEqual(ic.ichimoku_cloud()[0],self.all[-1][0])
-        self.assertIsInstance(ic.ichimoku_cloud()[0],float)
-        self.assertGreater(ic.ichimoku_cloud()[0],0)
-        #test_kijun_sen(self):        
-        self.assertAlmostEqual(ic.ichimoku_cloud()[1],self.all[-1][1])
-        self.assertIsInstance(ic.ichimoku_cloud()[1],float)
-        self.assertGreater(ic.ichimoku_cloud()[1],0)
-        #test_sekou_span_a        
-        self.assertAlmostEqual(ic.ichimoku_cloud()[2],self.all[-1][2])
-        self.assertIsInstance(ic.ichimoku_cloud()[2],float)
-        self.assertGreater(ic.ichimoku_cloud()[2],0)
-        #test_senkou_span_b        
-        self.assertAlmostEqual(ic.ichimoku_cloud()[3],self.all[-1][3])
-        self.assertIsInstance(ic.ichimoku_cloud()[3],float)
-        self.assertGreater(ic.ichimoku_cloud()[3],0)
-        #test_cikou_span_a        
-        self.assertAlmostEqual(ic.ichimoku_cloud()[4],self.all[-1][4])
-        self.assertIsInstance(ic.ichimoku_cloud()[4],float)
-        self.assertGreater(ic.ichimoku_cloud()[4],0)
-
-        #test ichimoku cloud
-        self.assertIsInstance(ic.ichimoku_cloud(), pd.Series)
-        self.assertEqual(len(ic.ichimoku_cloud()),len(self.all[-1]))
-        
     def test_input_value(self):
         self.assertIsNotNone(self.close[100])
         self.assertIsNotNone(self.low[0])
@@ -72,12 +68,8 @@ class TestIchimokuCloud(unittest.TestCase):
         self.assertIsInstance(self.close, list)
         self.assertIsInstance(self.low, list)
         self.assertIsInstance(self.high, list)
-        
-        
 
-
-
-
+    
 if __name__ == '__main__':
     unittest.main()
 
