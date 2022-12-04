f1c546c6eccf3c8623493c9bdf1323be73c699d2
Esra Ovalı <esra.ovali@gmail.com>
2022-12-04 19:30:05 +0300

    SMA is added (#75)
    * SMA is added
    
    * SMA is updated

---
+++ hyta/SMA.py
@@ -0,0 +1,14 @@
+import pandas as pd     
+ 
+class Sma:
+
+    def __init__(self,close,period):
+        self.df=pd.DataFrame(data={"close":close})
+        self.period=period
+
+
+    def Sma(self):
+        df = pd.DataFrame([self.df])
+        # calculate given period moving average using Pandas
+        df['sma'] = df['close'].rolling(self.period).mean()
+        return df['sma']
