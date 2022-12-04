34c0cb2056884008d60f90cc9fb9e161e222fc83
Esra Ovalı <esra.ovali@gmail.com>
2022-12-04 19:27:54 +0300

    SMA is updated

--- hyta/SMA.py
+++ hyta/SMA.py
@@ -1,15 +1,14 @@
-import pandas as pd     # needs pip install if not installed
+import pandas as pd     
  
 class Sma:
 
-    def __init__(self,close):
+    def __init__(self,close,period):
         self.df=pd.DataFrame(data={"close":close})
+        self.period=period
 
 
     def Sma(self):
         df = pd.DataFrame([self.df])
-        # calculate 3 moving average using Pandas
-        df['3sma'] = df['close'].rolling(3).mean()
-        # calculate 5 moving average using Pandas
-        df['5sma'] = df['close'].rolling(5).mean()
-        return df
+        # calculate given period moving average using Pandas
+        df['sma'] = df['close'].rolling(self.period).mean()
+        return df['sma']
