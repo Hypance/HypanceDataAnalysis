36c85fb615914604e30b3b753ec07b5a770dd6cd
Hasan Huseyin Boyaci <boyacihasanhuseyin@gmail.com>
2022-12-04 19:44:47 +0300

    kama is updated

--- hyta/kama.py
+++ hyta/kama.py
@@ -41,34 +41,35 @@ class KAMA:
         self.period = period
         self.fastest = fastest
         self.slowest = slowest
+        self.df = pd.DataFrame({"Close":self.close})
 
     def change(self):
-        df["Change"] = abs(df["Close"].diff(periods = 10))
-        return df["Change"]
+        self.df["Change"] = abs(self.df["Close"].diff(periods = 10))
+        return self.df["Change"]
 
     def volatility(self):
-        df["Volatility"] = abs(df["Close"].diff(periods = 1))
-        return df["Volatility"]
+        self.df["Volatility"] = abs(self.df["Close"].diff(periods = 1))
+        return self.df["Volatility"]
 
     def efficiency_ratio(self):
         self.change()
         self.volatility()
-        df["ER"] = df["Change"] / df["Volatility"].rolling(window = self.period).sum()
-        return df["ER"]
+        self.df["ER"] = self.df["Change"] / self.df["Volatility"].rolling(window = self.period).sum()
+        return self.df["ER"]
 
     def smoothing_constant(self):
         self.efficiency_ratio()
-        df["sc"] = ((df["ER"] * (2 / (self.fastest+1) - 2 / (self.slowest+1))) + (2 / (self.slowest+1))) ** 2
-        return df["sc"]
+        self.df["sc"] = ((self.df["ER"] * (2 / (self.fastest+1) - 2 / (self.slowest+1))) + (2 / (self.slowest+1))) ** 2
+        return self.df["sc"]
 
     def calc_kama(self):
         self.smoothing_constant()
-        df["kama"] = pd.Series()
+        self.df["kama"] = pd.Series()
         
-        for i in range(1,len(df["Close"])):
-            df["kama"][:self.period - 1] = np.nan
-            df["kama"].iloc[self.period - 1] = df["Close"].iloc[self.period - 1]
+        for i in range(1,len(self.df["Close"])):
+            self.df["kama"][:self.period - 1] = np.nan
+            self.df["kama"].iloc[self.period - 1] = self.df["Close"].iloc[self.period - 1]
 
-            if df["kama"][i] != np.nan :
-                df["kama"][i] = df["kama"][i-1] + df["sc"][i] * (df["Close"][i] - df["kama"][i-1])              
-        return df["kama"]
+            if self.df["kama"][i] != np.nan :
+                self.df["kama"][i] = self.df["kama"][i-1] + self.df["sc"][i] * (self.df["Close"][i] - self.df["kama"][i-1])              
+        return self.df["kama"]
