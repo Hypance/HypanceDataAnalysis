2322eafb2f9f35b3dfd55d2719df82ba95c0fed4
Esra Ovalı <esra.ovali@gmail.com>
2022-12-05 21:02:38 +0300

    CCI is updated

--- hyta/CCI.py
+++ hyta/CCI.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class CCI:
   def __init__(self,high,slow,close):
-    self.df=pd.DataFrame(data={'high':high,'slow':slow,'close':close)
+    self.df=pd.DataFrame(data={'high':high,'low':low,'close':close)
 
 #typical_price as TP
 
@@ -13,21 +13,21 @@ class CCI:
 #Simple_Moving_Average as SMA. It is calculated through TP.
   
   def SMA(self):
-    df = self.TP()
+    self.df['TP'] = self.TP(self.df)
     df['SMA'] = df['TP'].rolling().mean()
     return df['SMA']
 
 #Mean_Deviation as MD. It is calculated through TP.
 
   def MD(self):
-    df = self.TP()
+    self.df['TP'] = self.TP(self.df)
     df['MD']= df['TP'].rolling().apply(lambda x:pd.Series(x).MD())
     return df['MD']
   
 #Commodity_Channel_Index as CCI
 
   def CCI(self):
-    df['TP'] = self.TP()
+    self.df['TP'] = self.TP(self.df)
     df['CCI']= (df['TP']-df['SMA'])/(0.015*df['MD'])
     return df['CCI']   
     
