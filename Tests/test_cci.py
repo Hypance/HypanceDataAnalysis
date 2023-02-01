a233b1f80c99e8bca41afe6c09386e837592137c
Esra Ovalı <esra.ovali@gmail.com>
2023-02-01 17:07:46 +0300

    updated

--- Tests/test_cci.py
+++ Tests/test_cci.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from hyta.CCI import CCI
+from hyta.cci import CCI
 
     
 class TestCCI(unittest.TestCase):
@@ -17,36 +17,35 @@ class TestCCI(unittest.TestCase):
         self.high = self.data["High"]
         self.low = self.data["Low"]
         self.close = self.data["Close"]
-        self.CCI_data = CCI(self.high, self.low, self.close)
+        self.period=20
+        self.CCI_data = CCI(self.high, self.low, self.close, self.period)
         self.cci = CCI(
-            self.data["High"], self.data["Low"], self.data["Close"], period=20)
+            self.data["High"], self.data["Low"], self.data["Close"], self.period)
 
     def tearDown(self):
         del self.cci
         
   
-def test_TP(self):
-        self.assertAlmostEqual(
-            self.cci.TP()[24], self.data["TP"][24])
+    def test_TP(self):
+        self.assertAlmostEqual(self.cci.TP()[24], self.data["TP"][24])
         self.assertGreater(self.cci.TP()[25], -1)
         self.assertLess(self.cci.TP()[28], 30)
 
-def test_SMA(self):
-        self.assertAlmostEqual(
-            self.cci.SMA()[24], self.data["SMA"][24])
-        self.assertGreater(self.cci.SMA()[25], -1)
-        self.assertLess(self.cci.SMA()[28], 30)
+    def test_SMA(self):
+        tp=self.cci.TP()
+        self.assertAlmostEqual(self.cci.SMA(tp)[24], self.data["SMA"][24])
+        self.assertGreater(self.cci.SMA(tp)[25], -1)
+        self.assertLess(self.cci.SMA(tp)[28], 30)
 
 
-def test_MD(self):
-        self.assertAlmostEqual(
-            self.cci.MD()[23], self.data["MD"][23])
-        self.assertGreater(self.cci.MD()[27], -1)
-        self.assertLess(self.cci.MD()[28], 1)
+    def test_MAD(self):
+        tp=self.cci.TP()
+        self.assertAlmostEqual(self.cci.MAD(tp)[23], self.data["MAD"][23])
+        self.assertGreater(self.cci.MAD(tp)[27], -1)
+        self.assertLess(self.cci.MAD(tp)[28], 1)
 
-def test_CCI(self):
-        self.assertAlmostEqual(
-            self.cci.CCI()[24], self.data["CCI"][24])
+    def test_CCI(self):
+        self.assertAlmostEqual(self.cci.CCI()[24], self.data["CCI"][24])
         self.assertGreater(self.cci.CCI()[25], -1)
         self.assertLess(self.cci.CCI()[29], 1)
 
@@ -54,3 +53,4 @@ def test_CCI(self):
 if __name__ == "__main__":
     unittest.main()
         
+
