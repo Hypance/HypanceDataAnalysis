4e56bea0b0b52bf52099c4c0d6de90d5c942f252
Toygar Aksoy <toygar.aksoy@gmail.com>
2023-02-02 19:31:46 +0300

    mfi bug fix

--- Tests/test_mfi.py
+++ Tests/test_mfi.py
@@ -8,7 +8,7 @@ class TestCmf(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.data = pd.read_excel('files/cs-mfi.1.xls')
+        cls.data = pd.read_excel('Tests/files/cs-mfi.xls')
 
     @classmethod
     def tearDownClass(cls):
