14c7761130fd1be7dd69c412d71628a2f61bc63f
Bahar <bhrakay235@gmail.com>
2023-02-13 13:51:45 +0300

    Mine indicators run by Black

--- hyta/cmf.py
+++ hyta/cmf.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class CMF:
 
     """
@@ -22,29 +23,39 @@ class CMF:
         Returns the mfv.
 
     cmf(money_flow_multiplier,money_flow_volume,data):
-        Returns the cmf. 
+        Returns the cmf.
 
     """
-    def __init__(self,close:pd.Series,low:pd.Series,high:pd.Series,volume:pd.Series,period:int=20):
+
+    def __init__(
+        self,
+        close: pd.Series,
+        low: pd.Series,
+        high: pd.Series,
+        volume: pd.Series,
+        period: int = 20,
+    ):
         self.close = close
         self.low = low
         self.high = high
         self.volume = volume
         self.period = period
-        
+
     def money_flow_multiplier(self) -> pd.Series:
-        mfm = ((self.close - self.low) - (self.high - self.close)) / (self.high - self.low)
+        mfm = ((self.close - self.low) - (self.high - self.close)) / (
+            self.high - self.low
+        )
         try:
-            self.low/self.high
+            self.low / self.high
         except ZeroDivisionError as e:
             print("Value of high is zero:", e)
-        
+
         return mfm
 
     def money_flow_volume(self) -> pd.Series:
         mfv = self.money_flow_multiplier() * self.volume
         return mfv
-    
+
     def cmf(self) -> pd.Series:
         volume = self.volume
         period = self.period
