4af3a0dc7bb4340c60adeeeb82abe6ad38008637
Bahar <bhrakay235@gmail.com>
2023-02-13 16:48:58 +0300

    test ppo is changed

--- Tests/test_ppo.py
+++ Tests/test_ppo.py
@@ -18,16 +18,19 @@ class TestPPO(unittest.TestCase):
 
     def test_ema12(self):
         self.result_ema12 = self.ppo_data.ema_12()
-        self.assertAlmostEqual(self.result_ema12[1],self.ppo_data['EMA(12)'][1])
+        self.assertAlmostEqual(self.result_ema12[0],self.data["Close"][0])
+        self.assertAlmostEqual(self.result_ema12[1],self.data['EMA12'][1])
+        self.assertIsInstance(self.result_ema12,pd.DataFrame)
     
     def test_ema26(self):
         self.result_ema26 = self.ppo_data.ema_26()
-        self.assertAlmostEqual(self.result_ema26[1],self.ppo_data['EMA( 26)'][1])
+        self.assertAlmostEqual(self.result_ema26[3],self.data['EMA26'][3])
+        self.assertIsInstance(self.result_ema26,pd.DataFrame)
     
-    def ppo(self):
+    def test_ppo(self):
         self.result_ppo = self.ppo_data.ppo()
-        self.assertAlmostEqual(self.result_ppo[1],self.ppo_data['EMA( 26)'][1])
-        
+        self.assertAlmostEqual(self.result_ppo[1],self.data['PPO'][1])
+        self.assertIsInstance(self.result_ppo,pd.DataFrame)
 
 if __name__ == "__main__":
     unittest.main()
--- hyta/ppo.py
+++ hyta/ppo.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 class PPO:
@@ -6,37 +5,36 @@ class PPO:
         self.close = close
         self.ppo_data = pd.DataFrame({"Close":self.close})
 
-    def ema_12(self):
-        p_12 = round(2/(12+1),ndigits=6)
+    def ema_12(self) -> pd.DataFrame:
+        weight_p_12 = round(2/(12+1),ndigits=6)
        
         first = self.ppo_data["Close"][0]
-        ema_12_ilk = (self.ppo_data["Close"][1] * p_12) + (first * (1-p_12))
+        ema_12_ilk = (self.ppo_data["Close"][1] * weight_p_12) + (first * (1-weight_p_12))
 
         self.ppo_data["Ema_12"] = 0
         self.ppo_data["Ema_12"][:2] = [first,ema_12_ilk]
 
         for i in range(1,len(self.ppo_data["Close"])):
-            ema_12_ilk = (self.ppo_data["Close"][i] * p_12) + (self.ppo_data["Ema_12"][i-1] * (1-p_12))
+            ema_12_ilk = (self.ppo_data["Close"][i] * weight_p_12) + (self.ppo_data["Ema_12"][i-1] * (1-weight_p_12))
             self.ppo_data["Ema_12"][i:] = ema_12_ilk
         return pd.DataFrame(self.ppo_data["Ema_12"])
 
-    def ema_26(self):
-        p_26 = round(2/(26+1),ndigits=6)
+    def ema_26(self) -> pd.DataFrame:
+        weight_p_26 = round(2/(26+1),ndigits=6)
        
         first = self.ppo_data["Close"][0]
-        ema_26_ilk = (self.ppo_data["Close"][1] * p_26) + (first * (1-p_26))
+        ema_26_ilk = (self.ppo_data["Close"][1] * weight_p_26) + (first * (1-weight_p_26))
 
         self.ppo_data["Ema_26"] = 0
         self.ppo_data["Ema_26"][:2] = [first,ema_26_ilk]
 
         for i in range(1,len(self.ppo_data["Close"])):
-            ema_26_ilk = (self.ppo_data["Close"][i] * p_26) + (self.ppo_data["Ema_26"][i-1] * (1-p_26))
+            ema_26_ilk = (self.ppo_data["Close"][i] * weight_p_26) + (self.ppo_data["Ema_26"][i-1] * (1-weight_p_26))
             self.ppo_data["Ema_26"][i:] = ema_26_ilk
         return pd.DataFrame(self.ppo_data["Ema_26"])
     
-    def ppo(self):
-        # Bu kısımda AttributeError: 'Series' object has no attribute 'ema_12'
-        # hatası alıyorum. 
+    def ppo(self) -> pd.DataFrame:
         self.ema_12()
         self.ema_26()
-        return (self.ppo_data["Ema_12"] - self.ppo_data["Ema_26"]) / self.ppo_data["Ema_12"]
+        self.ppo_data["PPO"] = (self.ppo_data["Ema_12"] - self.ppo_data["Ema_26"]) / self.ppo_data["Ema_12"]
+        return pd.DataFrame(self.ppo_data["PPO"])
