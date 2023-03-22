ba780e240cf10556e2ff4f9ef729d36ed5e1b56c
Bahar <57152233+Bahar-K@users.noreply.github.com>
2023-03-22 20:57:16 +0300

    Adx (#114)
    * deneme_adx.ipynb is added
    
    * dx.py is added
    
    * dx is changed
    
    * adx file is added
    
    * dx indicator is remover in adx branch
    
    * bug fixed
    
    * setup variable is fixed
    
    * dx files is added
    
    * there is a bug
    
    * adx is upgraded
    
    * adx is upgraded
    
    * todo comment is added.

--- hyta/adx.py
+++ hyta/adx.py
@@ -1,79 +1,22 @@
+import pandas as pd
 import numpy as np
-import math
+from hyta.dx import DX
 
 class ADX:
-    def __init__(self, highs:list, lows:list, closes:list, periods:int=14):
-        self.highs = highs
-        self.lows = lows
-        self.closes = closes
-        self.periods = periods
+    def __init__(self, high, low, close, period=14):
+        self.data = pd.DataFrame({'high': high, 'low': low, 'close': close})
+        self.period = period
 
-    def __ema(self, arr, periods=14, weight=1, init=None):
-        leading_na = np.where(~np.isnan(arr))[0][0]
-        arr = arr[leading_na:]
-        alpha = weight / (periods + (weight-1))
-        alpha_rev = 1 - alpha
-        n = arr.shape[0]
-        pows = alpha_rev**(np.arange(n+1))
-        out1 = np.array([])
-        if 0 in pows:
-            out1 = self.__ema(arr[:int(len(arr)/2)], periods)
-            arr = arr[int(len(arr)/2) - 1:]
-            init = out1[-1]
-            n = arr.shape[0]
-            pows = alpha_rev**(np.arange(n+1))
-        scale_arr = 1/pows[:-1]
-        if init:
-            offset = init * pows[1:]
-        else:
-            offset = arr[0]*pows[1:]
-        pw0 = alpha*alpha_rev**(n-1)
-        mult = arr*pw0*scale_arr
-        cumsums = mult.cumsum()
-        out = offset + cumsums*scale_arr[::-1]
-        out = out[1:] if len(out1) > 0 else out
-        out = np.concatenate([out1, out])
-        out[:periods] = np.nan
-        out = np.concatenate(([np.nan]*leading_na, out))
-        return out
+    def adx(self):
+        dx_data = DX(self.data['high'], self.data['low'], self.data['close']).dx()
+        dx_mean = dx_data[self.period:(self.period+13)].mean()
 
+        self.data["ADX"] = np.nan
+        self.data["ADX"][27] = dx_mean
+        other_value = []
+        for i in range(28, len(dx_data)+1):
+            adx_result = ((self.data["ADX"][i-1] * (self.period - 1)) + dx_data[i-1]) / self.period
+            other_value.append(adx_result)
+            self.data["ADX"][28:] = other_value[i-28]
 
-    def __atr(self, highs, lows, closes, periods=14, ema_weight=1):
-        hi = np.array(highs)
-        lo = np.array(lows)
-        c = np.array(closes)
-        tr = np.vstack([np.abs(hi[1:]-c[:-1]),
-                        np.abs(lo[1:]-c[:-1]),
-                        (hi-lo)[1:]]).max(axis=0)
-        atr = self.__ema(tr, periods=periods, weight=ema_weight)
-        atr = np.concatenate([[np.nan], atr])
-        return atr
-
-
-    def adx(self, periods=14):
-        highs = self.highs
-        lows = self.lows
-        closes = self.closes
-
-        highs = np.array(highs)
-        lows = np.array(lows)
-        closes = np.array(closes)
-        up = highs[1:] - highs[:-1]
-        down = lows[:-1] - lows[1:]
-        up_idx = up > down
-        down_idx = down > up
-        updm = np.zeros(len(up))
-        updm[up_idx] = up[up_idx]
-        updm[updm < 0] = 0
-        downdm = np.zeros(len(down))
-        downdm[down_idx] = down[down_idx]
-        downdm[downdm < 0] = 0
-        _atr = self.__atr(highs, lows, closes, periods)[1:]
-        updi = 100 * self.__ema(updm, periods) / _atr
-        downdi = 100 * self.__ema(downdm, periods) / _atr
-        zeros = (updi + downdi == 0)
-        downdi[zeros] = .0000001
-        adx = 100 * np.abs(updi - downdi) / (updi + downdi)
-        adx = self.__ema(np.concatenate([[np.nan], adx]), periods)
-        adx = [None if math.isnan(x) else round(x, ndigits=2) for x in adx.tolist()]
-        return adx
+        return other_value[-1]
