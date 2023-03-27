362422c668b111724c8e540a9b5647877e6fb8aa
esra melike çakır <77747702+esramelikecakir@users.noreply.github.com>
2023-03-27 16:38:32 +0300

    Update test_Aroon.py

--- Tests/test_Aroon.py
+++ Tests/test_Aroon.py
@@ -2,13 +2,14 @@ import pandas as pd
 from hyta.aroon import Aroon
 import unittest
 
+
 class TestAroon(unittest.TestCase):
-    '''
+    """
         In this class, testing the following things:
         Element values are equal and is in the same index.
         Returns the same length data comparing to original data
         Returning list is same type as we want(pandas series)
-        '''
+        """
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -19,35 +20,35 @@ class TestAroon(unittest.TestCase):
         del cls.df
 
     def test_aroon_up(self):
-        originalAroonUp = pd.Series(self.df["Aroon Up"])
-        testAroonUp = Aroon(self.df["Adj Close"]).aroon_up()
+        original_aroon_up = pd.Series(self.df["Aroon Up"])
+        test_aroon_up = Aroon(self.df["Adj Close"]).aroon_up()
 
-        self.assertAlmostEqual(originalAroonUp[26], testAroonUp[25])
+        self.assertAlmostEqual(original_aroon_up[26], test_aroon_up[25])
 
-        self.assertEqual(len(originalAroonUp), len(testAroonUp))
+        self.assertEqual(len(original_aroon_up), len(test_aroon_up))
 
-        self.assertIsInstance(testAroonUp, pd.Series)
+        self.assertIsInstance(test_aroon_up, pd.Series)
 
     def test_aroon_down(self):
-        originalAroonDown = pd.Series(self.df["Aroon Down"])
-        testAroonDown = Aroon(self.df["Adj Close"]).aroon_down()
+        original_aroon_down = pd.Series(self.df["Aroon Down"])
+        test_aroon_down = Aroon(self.df["Adj Close"]).aroon_down()
 
-        self.assertAlmostEqual(originalAroonDown[26], testAroonDown[25])
-        
-        self.assertEqual(len(originalAroonDown), len(testAroonDown))
+        self.assertAlmostEqual(original_aroon_down[26], test_aroon_down[25])
 
-        self.assertIsInstance(testAroonDown, pd.Series)
+        self.assertEqual(len(original_aroon_down), len(test_aroon_down))
+
+        self.assertIsInstance(test_aroon_down, pd.Series)
 
 
     def test_aroon_oscilattor(self):
-        originalAroonOscilattor = pd.Series(self.df["Aroon Oscillator"])
-        testAroonOscilattor = Aroon(self.df["Adj Close"]).aroon_oscilattor()
+        original_aroon_oscilattor = pd.Series(self.df["Aroon Oscillator"])
+        test_aroon_oscilattor = Aroon(self.df["Adj Close"]).aroon_oscilattor()
 
-        self.assertAlmostEqual(originalAroonOscilattor[26], testAroonOscilattor[25])
+        self.assertAlmostEqual(original_aroon_oscilattor[26], test_aroon_oscilattor[25])
 
-        self.assertEqual(len(originalAroonOscilattor), len(testAroonOscilattor))
+        self.assertEqual(len(original_aroon_oscilattor), len(test_aroon_oscilattor))
 
-        self.assertIsInstance(testAroonOscilattor, pd.Series)
+        self.assertIsInstance(test_aroon_oscilattor, pd.Series)
 
     if __name__ == "_main_":
          unittest.main()
