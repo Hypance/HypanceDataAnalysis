7c16399fd5e0cea812c1fc4c112e9edabe8c6a42
Ugur Aydin <uguraydin7541@gmail.com>
2023-03-28 21:19:15 +0300

    black method

--- hyta/on_balance_volume.py
+++ hyta/on_balance_volume.py
@@ -11,27 +11,23 @@ class OnBalanceVolume:
     If today’s close is equal to yesterday’s close then:
     OBV = Yesterday’s OBV
     """
-    def __init__(self,close_list,volume_list,period):
+
+    def __init__(self, close_list, volume_list, period):
         self.close_list = close_list
         self.volume_list = volume_list
         self.period = period
 
     def on_balance_volume(self):
-
-        main_obv = self.volume_list[-self.period] 
-        #first day OBV is first day value
-        for main_period in range(self.period-1,0,-1):                      
-                
-            if self.close_list[-main_period] > self.close_list[-main_period-1]:
-                
+        main_obv = self.volume_list[-self.period]
+        # first day OBV is first day value
+        for main_period in range(self.period - 1, 0, -1):
+            if self.close_list[-main_period] > self.close_list[-main_period - 1]:
                 main_obv += self.volume_list[-main_period]
 
-            elif self.close_list[-main_period] < self.close_list[-main_period-1]:
-                
+            elif self.close_list[-main_period] < self.close_list[-main_period - 1]:
                 main_obv -= self.volume_list[-main_period]
 
-            elif self.close_list[-main_period] == self.close_list[-main_period-1]:
-                
+            elif self.close_list[-main_period] == self.close_list[-main_period - 1]:
                 main_obv += 0
 
         return main_obv
