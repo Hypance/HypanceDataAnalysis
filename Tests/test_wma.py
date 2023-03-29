a504db57d159da9ae1a77e12f148886504061dc2
Ugur Aydin <uguraydin7541@gmail.com>
2023-03-29 10:55:33 +0300

    black method

--- Tests/test_wma.py
+++ Tests/test_wma.py
@@ -4,13 +4,13 @@ from hyta.wma import WeightedMovingAverage
 
 
 class TestWeightedMovingAverage(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        cls.data = pd.read_excel('Tests/files/wma.xlsx')
-        cls.close = cls.data['close'].to_list()
-        cls.period= cls.data['period'].to_list()
-        cls.result = cls.data['result'].to_list()
+        cls.data = pd.read_excel("Tests/files/wma.xlsx")
+        cls.close = cls.data["close"].to_list()
+        cls.period = cls.data["period"].to_list()
+        cls.result = cls.data["result"].to_list()
+
     @classmethod
     def tearDownClass(cls):
         del cls.data
@@ -19,23 +19,21 @@ class TestWeightedMovingAverage(unittest.TestCase):
         del cls.result
 
     def test_input_value(self):
-        #period is always less than the lenght of close price list
-        self.assertGreater(len(self.close),int(self.period[11]))
-        self.assertLess(self.period[3],len(self.close))
-        #close list type have to be list
-        self.assertIsInstance(self.close, list)        
-        #period is never 0 and period and close's value is never None
-        self.assertNotEqual(self.period[7],0)
+        # period is always less than the lenght of close price list
+        self.assertGreater(len(self.close), int(self.period[11]))
+        self.assertLess(self.period[3], len(self.close))
+        # close list type have to be list
+        self.assertIsInstance(self.close, list)
+        # period is never 0 and period and close's value is never None
+        self.assertNotEqual(self.period[7], 0)
         self.assertIsNotNone(self.period[0])
         self.assertIsNotNone(self.close[29])
-        
-
 
     def test_wma(self):
-        wma = WeightedMovingAverage(self.close,int(self.period[1]))
-        #check result value
-        self.assertAlmostEqual(wma.weighted_moving_average(),self.result[1],2)
+        wma = WeightedMovingAverage(self.close, int(self.period[1]))
+        # check result value
+        self.assertAlmostEqual(wma.weighted_moving_average(), self.result[1], 2)
         self.assertNotAlmostEqual(wma.weighted_moving_average(), self.result[9])
-        #check result type
-        self.assertIsInstance(wma.weighted_moving_average(),float)
-        self.assertNotIsInstance(wma.weighted_moving_average(), str)        
+        # check result type
+        self.assertIsInstance(wma.weighted_moving_average(), float)
+        self.assertNotIsInstance(wma.weighted_moving_average(), str)
