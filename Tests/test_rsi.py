12e71787bb7b0bd256b4c5d5df750e68e9e23d0d
esramelikecakir <esramelike10@gmail.com>
2023-04-02 14:29:52 +0300

    rsi.black

---
+++ Tests/test_rsi.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from hyta.rsi import RSI
+import unittest
+
+
+class TestRSI(unittest.TestCase):
+    """
+    In this class, testing the following things:
+    Element values are equal and is in the same index.
+    Returns the same length data comparing to original data
+    Returning list is same type as we want(pandas series)
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.df = pd.read_excel("Tests/files/RSI.xlsx")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        del cls.df
+
+    def test_gain_calculator(self):
+        originalGAIN = pd.Series(self.df["Avg. Gain"])
+        testGAIN = RSI(self.df["Close"]).gain_calculator()
+
+        self.assertAlmostEqual(originalGAIN[14], testGAIN[13])
+
+        self.assertAlmostEqual(originalGAIN.iloc[-1], testGAIN.iloc[-1], places=7)
+
+        self.assertEqual(len(originalGAIN), len(testGAIN))
+
+        self.assertIsInstance(testGAIN, pd.Series)
+
+    def test_loss_calculator(self):
+        originalLOSS = pd.Series(self.df["Avg. Loss"])
+        testLOSS = RSI(self.df["Close"]).loss_calculator()
+
+        self.assertAlmostEqual(originalLOSS[14], testLOSS[13])
+
+        self.assertAlmostEqual(originalLOSS.iloc[-1], testLOSS.iloc[-1], places=7)
+
+        self.assertEqual(len(originalLOSS), len(testLOSS))
+
+        self.assertIsInstance(testLOSS, pd.Series)
+
+    def test_rsi(self):
+        originalRsi = pd.Series(self.df["RSI"])
+        testRsi = RSI(self.df["Close"]).rsi()
+
+        self.assertAlmostEqual(originalRsi[14], testRsi[13])
+
+        self.assertAlmostEqual(originalRsi.iloc[-1], testRsi.iloc[-1], places=7)
+
+        self.assertEqual(len(originalRsi), len(testRsi))
+
+        self.assertIsInstance(testRsi, pd.Series)
+
+    if __name__ == "_main_":
+        unittest.main()
--- hyta/rsi.py
+++ hyta/rsi.py
@@ -1,35 +1,65 @@
 import pandas as pd
-class Rsi_counter():
-
-    def __init__(self, data_close, total_gain, total_loss):
-        self.data_close = data_close
-        self.total_loss = total_loss
-        self.total_gain = total_gain
+import numpy as np
 
 
-    def gain_calculator(self):
-        total_gain = 0
-        for i in range(0, 14):
-            if data_close[i+1] > data_close[i]:
-                gain = (data_close[i+1] - data_close[i])
-                total_gain = total_gain + gain
-        return total_gain
+class RSI:
+    """
+    This class calculates RSI for period = 14.
+    Attributes
+    -----------
+    close : pandas
+    period : int
+    """
 
+    def __init__(self, close, period: int = 14) -> None:
+        self.close = close
+        self.period = period
 
-    def loss_calculator(self):
-        total_loss = 0
-        for i in range(0, 14):
-            if data_close[i] > data_close[i+1]:
-                loss = data_close[i] - data_close[i+1]
-                total_loss = total_loss + loss
-        return total_loss
+    def gain_calculator(self) -> pd.Series:
+        """
+        From period, returns a pandas series
+        by using gain_calculator function.
+        Sum of the difference between the current closes
+        and previous closes when the current closes are less than previous closes.
+        Then diveded to 14.
+        """
+        close = self.close
+        gain_list = pd.Series(0.0, index=close.index)
+        for i in range(1, len(close)):
+            if close[i] > close[i - 1]:
+                gain = close[i] - close[i - 1]
+                gain_list[i] = gain
+        gain_avg = gain_list.rolling(window=self.period).mean()
 
-    def rsi(self):
-        total_gain = self.gain_calculator()
-        total_loss =self.loss_calculator()
-        rs = total_gain/total_loss
-        rsi = 100 - (100/(1+rs))
+        return gain_avg
+
+    def loss_calculator(self) -> pd.Series:
+        """
+        From period, returns a pandas series
+        by using loss_calculator function.
+        Sum of the difference between the current closes
+        and previous closes when the current closes are greater than previous closes.
+        Then diveded to 14.
+        """
+        close = self.close
+        loss_list = pd.Series(0.0, index=close.index)
+        for i in range(1, len(close)):
+            if close[i] < close[i - 1]:
+                loss = close[i - 1] - close[i]
+                loss_list[i] = loss
+        loss_avg = loss_list.rolling(window=self.period).mean()
+
+        return loss_avg
+
+    def rsi(self) -> pd.Series:
+        """
+        In this function, it calculates rs by using gain/loss
+        and rsi by using 100-(100/(1+rs)).
+        Returns a pandas series.
+        """
+        gain = self.gain_calculator()
+        loss = self.loss_calculator()
+        rs = gain / loss
+        rs[rs == float("inf")] = 0
+        rsi = 100 - (100 / (1 + rs))
         return rsi
-
-
-
