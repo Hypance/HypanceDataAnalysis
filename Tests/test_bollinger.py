d93c21e0b83cf0de5bb71d32473f008ae6973ccb
Hasan Huseyin Boyaci <boyacihasanhuseyin@gmail.com>
2023-04-09 22:04:20 +0300

    excel files paths updated

--- Tests/test_bollinger.py
+++ Tests/test_bollinger.py
@@ -2,12 +2,12 @@ import unittest
 import pandas as pd
 from hyta.bollinger import Bollinger
 
-class TestBollinger(unittest.TestCase):
 
+class TestBollinger(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.df = pd.read_excel("Tests/files/bollinger.xlsx")
-        cls.indicator = Bollinger(cls.df,cls.df["Close"])
+        cls.indicator = Bollinger(cls.df, cls.df["Close"])
 
     @classmethod
     def tearDownClass(cls):
@@ -15,19 +15,27 @@ class TestBollinger(unittest.TestCase):
         del cls.indicator
 
     def test_bollinger(self):
-        pd.testing.assert_series_equal(self.df["Upper"],self.indicator.bands()[0],check_names=False)
-        pd.testing.assert_series_equal(self.df["Lower"],self.indicator.bands()[1],check_names=False)
-        
+        pd.testing.assert_series_equal(
+            self.df["Upper"], self.indicator.bands()[0], check_names=False
+        )
+        pd.testing.assert_series_equal(
+            self.df["Lower"], self.indicator.bands()[1], check_names=False
+        )
+
         # Checks if the upper band is greater than the lower band
-        self.assertGreaterEqual(self.indicator.bands()[0][25],self.indicator.bands()[1][25])
-        self.assertGreaterEqual(self.indicator.bands()[0][24],self.indicator.bands()[1][24])
+        self.assertGreaterEqual(
+            self.indicator.bands()[0][25], self.indicator.bands()[1][25]
+        )
+        self.assertGreaterEqual(
+            self.indicator.bands()[0][24], self.indicator.bands()[1][24]
+        )
 
-        self.assertIsInstance(self.indicator.bands()[0],pd.Series)
-        self.assertIsInstance(self.indicator.bands()[1],pd.Series)
+        self.assertIsInstance(self.indicator.bands()[0], pd.Series)
+        self.assertIsInstance(self.indicator.bands()[1], pd.Series)
 
-        self.assertEqual(len(self.df),len(self.indicator.bands()[0].tolist()))
-        self.assertEqual(len(self.df),len(self.indicator.bands()[1].tolist()))
+        self.assertEqual(len(self.df), len(self.indicator.bands()[0].tolist()))
+        self.assertEqual(len(self.df), len(self.indicator.bands()[1].tolist()))
+
 
 if __name__ == "__main__":
     unittest.main()
-    
