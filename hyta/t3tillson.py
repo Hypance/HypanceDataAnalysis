cbfea29adb3136e02e9c1f5af7affd152fe0899c
esra melike çakır <77747702+esramelikecakir@users.noreply.github.com>
2023-04-09 20:24:41 +0300

    Update t3tillson.py

--- hyta/t3tillson.py
+++ hyta/t3tillson.py
@@ -1,42 +1,40 @@
 from hyta.ema import EMA
 import numpy as np
 import pandas as pd
+
+
 class t3Tillson:
     """
-     This class calculates T3 Tillson Average indicator for period = 9.
-     Attributes
-     -----------
-     close : pandas
-     period : int
-     a : 0.618
-     c1 : -a**3
-     c2 : 3*(a**2)+ 3*(a**3)
-     c3 : -6(a**2) - 3*a - 3*(a**3)
-     c4 : 1 + 3*a + a**3 + 3*(a**2)
-      """
+    Calculates the T3 Tillson indicator for a given set of closing prices.
+    It uses six exponential moving averages
+    calculated using the "EMA" class from the "ema" module.
+    The class constructor takes a list or array of
+    closing prices as "close" and an optional integer as "period" (default value is 9)
+    that specifies the number of periods to use in the calculation.
+    """
 
     def __init__(self, close, period: int = 9) -> None:
-         self.period = period
-         self.a = 0.618
-         self.c1 = -self.a ** 3
-         self.c2 = 3 * (self.a ** 2) + 3 * (self.a ** 3)
-         self.c3 = -6*(self.a ** 2) - 3 * self.a - 3 * (self.a ** 3)
-         self.c4 = 1 + 3 * self.a + self.a ** 3 + 3 * (self.a ** 2)
-         self.df = pd.DataFrame(data={"Close": close})
-         self.close = self.df["Close"]
-         self.e1 = self.ema1()
-         self.e2 = self.ema2()
-         self.e3 = self.ema3()
-         self.e4 = self.ema4()
-         self.e5 = self.ema5()
-         self.e6 = self.ema6()
+        self.period = period
+        self.a = 0.618
+        self.c1 = -self.a**3
+        self.c2 = 3 * (self.a**2) + 3 * (self.a**3)
+        self.c3 = -6 * (self.a**2) - 3 * self.a - 3 * (self.a**3)
+        self.c4 = 1 + 3 * self.a + self.a**3 + 3 * (self.a**2)
+        self.df = pd.DataFrame(data={"Close": close})
+        self.close = self.df["Close"]
+        self.e1 = self.ema1()
+        self.e2 = self.ema2()
+        self.e3 = self.ema3()
+        self.e4 = self.ema4()
+        self.e5 = self.ema5()
+        self.e6 = self.ema6()
 
     def ema1(self) -> pd.Series:
-         """
-         Calculates ema1 using calc_ema from EMA according to close and period.
-         """
-         e1 = EMA(self.close, self.period).calc_ema()
-         return e1
+        """
+        Calculates ema1 using calc_ema from EMA according to close and period.
+        """
+        e1 = EMA(self.close, self.period).calc_ema()
+        return e1
 
     def ema2(self) -> pd.Series:
         """
@@ -77,5 +75,10 @@ class t3Tillson:
         """
         Calculates T3 Tillson using the c1*e6 + c2*e5 + c3*e4 + c4*e3 formula
         """
-        T3Tillson = self.c1 * self.e6 + self.c2 * self.e5 + self.c3 * self.e4 + self.c4 * (self.e3)
+        T3Tillson = (
+            self.c1 * self.e6
+            + self.c2 * self.e5
+            + self.c3 * self.e4
+            + self.c4 * self.e3
+        )
         return T3Tillson
