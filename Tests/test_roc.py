b00ba05f7ba58682e14a8f0b1668bdc33b848e51
Toygar Aksoy <toygar.aksoy@gmail.com>
2023-04-10 19:23:24 +0300

    fixed and linted

--- Tests/test_roc.py
+++ Tests/test_roc.py
@@ -1,42 +1,41 @@
 import unittest
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from hyta.roc import ROC
+
 
 class TestRateOfChange(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.df = pd.read_excel("files/ROC.xlsx")
+        cls.df = pd.read_excel("Tests/files/ROC.xlsx")
         cls.close = np.array(cls.df["close"])
-    
+
     @classmethod
     def tearDownClass(cls) -> None:
         del cls.df
-    
-    '''
+
+    """
     In this module, testing the following things:
     Element values are equal and is in the same index.
     Returns the same length data comparing to original data
     Returning list is same type as we want(np.ndarray)
     [12:] is necessary to skip the empty values of ROC in Sample Excel.
-    '''
-
+    """
 
     def test_Roc(self):
-        
+
         original = np.array(self.df["ROC"][12:])
-        
+
         test = ROC(self.close).roc()
-        
+
         self.assertIsNotNone(test)
-                
-        self.assertAlmostEqual(original[-1],test[-1]) 
-        
-        self.assertEqual(len(original),len(test)) 
 
-        self.assertIsInstance(test,pd.Series)
+        self.assertAlmostEqual(original[-1], test.iloc[-1])
 
- 
+        self.assertEqual(len(original), len(test))
+
+        self.assertIsInstance(test, pd.Series)
+
 
 if __name__ == "__main__":
     unittest.main()
--- hyta/roc.py
+++ hyta/roc.py
@@ -1,30 +1,31 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 
 
 class ROC:
-    '''
+    """
     This class calculate ROC for default 12-periods.
 
     It needs to get 'close' values respectively.
 
-    '''
-    def __init__(self,close,period:int=12) -> None:
+    """
+
+    def __init__(self, close, period: int = 12) -> None:
         self.close = close
         self.period = period
 
-    
-
     def roc(self):
-        '''
+        """
         This method calculates ROC by using
         ' ROC = [(Close - Close n periods ago) / (Close n periods ago)] * 100 '
         this equation.
         Returns ROC array without nan values.
         Returns a np.ndarray.
-        '''
+        """
         ROC = np.array([])
-        for i in range(len(self.close)-self.period):
-            ROC = np.append(ROC,((self.close[i+self.period] - self.close[i]) / self.close[i])*100)
+        for i in range(len(self.close) - self.period):
+            ROC = np.append(
+                ROC,
+                ((self.close[i + self.period] - self.close[i]) / self.close[i]) * 100,
+            )
         return pd.Series(ROC)
-        
