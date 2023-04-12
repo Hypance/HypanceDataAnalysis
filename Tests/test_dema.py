f14043d010d7e473d28f7326b3b7cfd76d7fff30
esra melike çakır <77747702+esramelikecakir@users.noreply.github.com>
2023-04-12 13:04:06 +0300

    Update test_dema.py

--- Tests/test_dema.py
+++ Tests/test_dema.py
@@ -19,7 +19,7 @@ class TestDema(unittest.TestCase):
     def tearDownClass(cls) -> None:
         del cls.df
 
-    def test_ema1(self):
+    def test_ema(self):
         original_ema = pd.Series(self.df["ema"])
         test_ema = DEMA(self.df["close"]).ema()
         self.assertAlmostEqual(original_ema[150], test_ema[150], places=7)
@@ -27,7 +27,7 @@ class TestDema(unittest.TestCase):
         self.assertEqual(len(original_ema), len(test_ema))
         self.assertIsInstance(test_ema, pd.Series)
 
-    def test_ema2(self):
+    def test_dema(self):
         original_dema = pd.Series(self.df["dema"])
         test_dema = DEMA(self.df["close"]).dema()
         self.assertAlmostEqual(original_dema[100], test_dema[100], places=7)
